7a04543e7f64a4778bc9041ce48911c9bce3e187
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-09-24 17:12:24 +0000

    added grandmother.py

---
+++ grandmother.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Daria Bernice Calitis
+# Created on: Sept 2021
+# This program is the grandmother's blessing giver program
+
+
+def main():
+    # This function checks if the user is rich or handsome
+    is_rich = input("Are you rich?: ")
+    is_handsome = input("Are you good looking?: ")
+
+    if is_rich == "yes" and is_handsome == "yes":
+        print("You can absolutely date my grandchild!")
+    elif is_rich == "yes":
+        print("You can date my grandchild with your riches!")
+    elif is_handsome == "yes":
+        print("You and my grandchild can make a beautiful pair!")
+    elif is_rich == "no" and is_handsome == "no":
+        print("Then, you can not date my grandchild!")
+    else:
+        print("Invalid Input. (Yes and no answers must be lowercase)")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
